36bc3b0ceb8b819e971d16ed3b526d97110f1e85
wardiman12 <simpangbedeng@gmail.com>
2025-07-17 13:59:19 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#295484',
+      }
+    },
+  },
+  plugins: [],
+}
+
